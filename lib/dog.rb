c5f19d4c2045d9ff70cc80cbe0ea57121fca8343
Parker Williamson <parker.williamson@gmail.com>
2020-05-19 21:30:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,4 +6,13 @@ class Dog
   @@all = []
   
   def initialize (name, owner)
+    @name = name 
+    @owner = owner 
+    @mood = "nervous"
+    @@all << self
+  end
+  
+  def self.all 
+    @@all 
+  end
 end
